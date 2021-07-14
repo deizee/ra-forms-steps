492c35be88b36fa7853df08b4d18cd41339f0b8c
deizee <deizee@mail.ru>
2021-07-14 19:42:52 +0500

    done

---
+++ src/components/Record.js
@@ -0,0 +1,24 @@
+const Record = (props) => {
+    const {data, onDel/*, onEdit*/} = props;
+
+    const onDelRecord = () => {
+        onDel(data.id);
+    }
+
+    // const onEditRecord = () => {
+    //     onEdit(data.date, data.dist);
+    // }
+
+    return (
+        <div className="Record">
+            <span className="Widget-Date">{data.date}</span>
+            <span className="Widget-Dist">{data.dist}</span>
+            <div className="Widget-Actions">
+                <button className="Edit" /*onClick={onEditRecord}*/>✎</button>
+                <button className="Delete" onClick={onDelRecord}>✘</button>
+            </div>
+        </div>
+    )
+}
+
+export default Record;
--- src/components/TrainingList.js
+++ src/components/TrainingList.js
@@ -1,7 +1,22 @@
-const TrainingList = () => {
+import Record from "./Record";
+
+const TrainingList = (props) => {
+    const { records, onDelete } = props;
+    const sorted = records.sort((a, b) =>  b.dateFormat - a.dateFormat);
+    const onDelHandler = (id) => {
+        onDelete(id);
+    }
+
     return (
         <div className="TrainingList">
-
+            <div className="List-Header">
+                <span>Дата (ДД.ММ.ГГ)</span>
+                <span>Пройдено км</span>
+                <span>Действия</span>
+            </div>
+            <div className="List-Table">
+                {sorted.map(record => <Record key={record.id} data={record} onDel={onDelHandler} />)}
+            </div>
         </div>
     )
 }
