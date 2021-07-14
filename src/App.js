04fedecd739d361c6f17244fef991d9dd97c0c66
deizee <deizee@mail.ru>
2021-07-14 18:15:55 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TrainingList from "./components/TrainingList";
+
+function App() {
+  return (
+    <div className="App">
+      <TrainingList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TrainingList.js
@@ -0,0 +1,9 @@
+const TrainingList = () => {
+    return (
+        <div className="TrainingList">
+
+        </div>
+    )
+}
+
+export default TrainingList;
